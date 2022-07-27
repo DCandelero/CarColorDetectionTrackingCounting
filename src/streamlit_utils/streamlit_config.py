2166e9ac19cd5047c5f1a2bdc379078687cdf862
DCandelero <davidcandelero557@gmail.com>
2022-07-26 23:09:01 -0300

    Add streamlit config and functions files

---
+++ src/streamlit_utils/streamlit_config.py
@@ -0,0 +1,64 @@
+import streamlit as st
+from streamlit_utils import streamlit_downloads
+
+def set_streamlit_layout():
+    # Streamlit page config
+    st.set_page_config(page_title='Car&Color Detection', page_icon=':car', layout="wide")
+    st_cols = st.columns([8,1,1,1,1,1])
+    st_video = st_cols[0].empty()
+    st_df = st_cols[0].empty()
+    # Streamlit sidebar config
+    # Sidebar Control
+    st_control_option = st.sidebar.selectbox('Control Comands', ['Stop', 'Start', 'Restart'])
+    # Sidebar Upload
+    st_upload_option = st.sidebar.selectbox('Choose your upload option', ['Sample video', 'Youtube link', 'File upload'])
+    if(st_upload_option == 'Youtube link'):
+        st_upload_option_value = st.sidebar.text_input('Paste youtube link here!')
+    elif(st_upload_option == 'File upload'):
+        st_upload_option_value = st.sidebar.file_uploader('Upload your mp4 file here!')
+    else:
+        st.sidebar.write('Sample video is being used')
+        st_upload_option_value = '../Data/Traffic_Example.mp4'
+    # Sidebar Download
+    st_download_flag= st.sidebar.select_slider('Enable data download', options=['No', 'Yes'])
+    if(st_download_flag == 'Yes'):
+        st_csv_download_option = st.sidebar.checkbox('CSV DataFrame')
+        st_zip_images_download_option = st.sidebar.checkbox('Last car 30 images (PNG)')
+    else:
+        st_csv_download_option = 0
+        st_zip_images_download_option = 0
+    st_df_download = st.sidebar.empty() # DataFrame Download
+    st_zip_download = st.sidebar.empty() # Zipped images Download
+
+    return (st_cols, st_video, st_df, st_control_option, st_download_flag, st_csv_download_option, 
+        st_zip_images_download_option, st_df_download, st_zip_download, st_upload_option_value)
+
+
+# # remove hamburger menu top left side
+# st.markdown(""" <style>
+# #MainMenu {visibility: hidden;}
+# footer {visibility: hidden;}
+# </style> """, unsafe_allow_html=True)
+
+
+# # remove layout padding
+# padding = 0
+# st.markdown(f""" <style>
+#     .reportview-container .main .block-container{{
+#         padding-top: {padding}rem;
+#         padding-right: {padding}rem;
+#         padding-left: {padding}rem;
+#         padding-bottom: {padding}rem;
+#     }} </style> """, unsafe_allow_html=True)
+
+
+# https://blog.streamlit.io/introducing-theming/
+# https://docs.streamlit.io/library/advanced-features/configuration
+# create a .streamlit folder in my repository and add this config.toml file
+# # Custom color pallete
+# [theme]
+# primaryColor="#2214c7"
+# backgroundColor="#ffffff"
+# secondaryBackgroundColor="#e8eef9"
+# textColor="#000000"
+# font="sans serif"
---
+++ src/streamlit_utils/streamlit_downloads.py
@@ -0,0 +1,69 @@
+import base64
+import pandas as pd
+import io
+from zipfile import ZipFile
+
+
+# Get download link for strings and pandas dataframe
+def get_download_link(object_to_download, download_filename, download_link_text):
+
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+# Get download link for image
+def get_image_download_link(img,filename,text):
+    buffered = io.BytesIO()
+    img.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+
+    return f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
+
+
+
+
+def get_zipped_images_download_link(img_list,filename,text):
+    zip_file_bytes_io = io.BytesIO()
+
+    with ZipFile(zip_file_bytes_io, 'w') as zip_file:
+        buffered = io.BytesIO()
+
+        for img_name, img in img_list:
+            img.save(buffered, format="PNG")
+            zip_file.writestr(img_name+".png", buffered.getvalue())
+    
+    zip_imgs_str = base64.b64encode(zip_file_bytes_io.getvalue()).decode()
+    
+    return f'<a href="data:file/txt;base64,{zip_imgs_str}" download="{filename}">{text}</a>'
+
+
+# Get csv data for download
+def get_csv_download_data(object_to_download):
+
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+
+    return object_to_download
+
+# Get image data for download
+def get_image_download_data(img):
+    buffered = io.BytesIO()
+    img.save(buffered, format="PNG")
+
+    return buffered
+
+# Get images zipped data for download
+def get_zipped_images_download_data(img_list):
+    zip_file_bytes_io = io.BytesIO()
+
+    with ZipFile(zip_file_bytes_io, 'w') as zip_file:
+        buffered = io.BytesIO()
+
+        for img_name, img in img_list:
+            img.save(buffered, format="PNG")
+            zip_file.writestr(img_name+".png", buffered.getvalue())
+    
+    return zip_file_bytes_io
